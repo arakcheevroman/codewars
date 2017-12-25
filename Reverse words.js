f751e951ae6a8e0ee7f1325ee093da6f6a304fc3
arakcheevroman <34820608+arakcheevroman@users.noreply.github.com>
2017-12-25 11:02:08 +0300

    Create Reverse words.js

---
+++ Reverse words.js
@@ -0,0 +1,21 @@
+function reverseWords(str) {
+  
+    var massOne = str.split(""),
+        massTwo = str.split(" "),
+        massTwoReverse = [],
+        resultTwo = [],
+        reg = /\s/;
+  
+    if (reg.test(str)) {
+        for (let i = 0; i < massTwo.length; i++) {
+            massTwoReverse.push(massTwo[i].split("").reverse());
+        }
+        for (let i = 0; i < massTwoReverse.length; i++) {
+            resultTwo[i] = massTwoReverse[i].join("");
+        }
+        return resultTwo.join(" ");
+    } else {
+        return massOne.reverse().join("");
+    }
+
+}
